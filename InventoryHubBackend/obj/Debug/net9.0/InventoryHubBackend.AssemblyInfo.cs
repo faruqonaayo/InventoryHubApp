72af04ee5075d1c958f035dfba346ff3054893e7
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-02-22 11:13:34 -0700

    Improve the product structure by adding category property

--- InventoryHubBackend/obj/Debug/net9.0/InventoryHubBackend.AssemblyInfo.cs
+++ InventoryHubBackend/obj/Debug/net9.0/InventoryHubBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryHubBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ce236df4b77c70d24a5688cef369ba75d06820")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryHubBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryHubBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

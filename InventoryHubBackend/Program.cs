f1ce236df4b77c70d24a5688cef369ba75d06820
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-02-22 10:44:06 -0700

    Initial Commit: Complete the integration of the fronted and backend application. Product list can be fetched and rendered on the client

---
+++ InventoryHubBackend/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed((host) => true);
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+
+app.MapGet("/api/products", () =>
+{
+    return new[]
+    {
+        new { Id = 1, Name = "Laptop", Price = 1200.50, Stock = 25 },
+        new { Id = 2, Name = "Headphones", Price = 50.00, Stock = 100 }
+    };
+});
+
+app.Run();
